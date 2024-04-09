39935cc133901ec555b04bccd337fda295a1b9ca
Hassan Fadel <hassan.m.fadel@hotmail.com>
2024-04-09 07:54:03 +0300

    empty commit

---
+++ backup-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#define vars
+time=$($date +%m-%d-%y_%H_%M_%s)
+#time=`$date +%m-%d-%y_%H_%M_%s``
+bachup_file=$1
+Dest=/home/ubuntu/backup
+filename=file-backup-$time.tar.gz
+LOG_FILE="/home/ubuntu/backup/logfile.log"
+#
